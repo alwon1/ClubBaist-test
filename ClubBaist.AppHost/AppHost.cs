3b02ec800e117acdfb8f1586c7fcde1f6b4fa450
Allyn Nelson <anelson28@nait.ca>
2026-03-30 22:20:37 -0600

    move files

---
+++ ClubBaist.AppHost/AppHost.cs
@@ -0,0 +1,31 @@
+using Azure.Provisioning.AppService;
+using Microsoft.Extensions.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+builder.Configuration["Azure:Location"] ??= "westus2";
+var appServiceEnv = builder.AddAzureAppServiceEnvironment("app-service-env");
+// appServiceEnv.ConfigureInfrastructure(infra =>
+// {
+//     var resources = infra.GetProvisionableResources();
+//     var plan = resources.OfType<AppServicePlan>().Single();
+
+//     plan.Sku = new AppServiceSkuDescription
+//     {
+//         Name = "F1",
+//         Tier = "Free"
+//     };
+// });
+var db = builder.AddAzureSqlServer("sql");
+db.RunAsContainer();
+//db.WithLifetime(ContainerLifetime.Persistent);
+var sql = db.AddDatabase("clubbaist");
+var seeder = builder.AddProject<Projects.ClubBaist_Seeder>("seeder")
+    .WithReference(sql)
+    .WaitFor(sql);
+
+builder.AddProject<Projects.ClubBaist_Web>("web").PublishAsAzureAppServiceWebsite()
+    .WithExternalHttpEndpoints()
+    .WithReference(sql)
+    .WaitFor(seeder);
+
+builder.Build().Run();
